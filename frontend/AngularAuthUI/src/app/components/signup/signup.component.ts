957cab267bf437cd2f2200d1c45b93c90881d298
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-07 23:20:04 +0600

    manage invalid formfield message for signup form and refactoring login form error message showing condition

--- frontend/AngularAuthUI/src/app/components/signup/signup.component.ts
+++ frontend/AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,18 +11,46 @@ export class SignupComponent implements OnInit {
   passwordFieldType : string = "password"
   isFieldTypeText: boolean = false;
   eyeIcon: string = "fa fa-eye-slash";
+  signUpForm!: FormGroup;
 
-  constructor(){
-
+  constructor(private fb : FormBuilder){
   }
   
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: [''],
+      email: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   hideShowPass(){
     this.isFieldTypeText = !this.isFieldTypeText;
     this.isFieldTypeText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
     this.isFieldTypeText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+  }
+
+  onSignUp(){
+    if(this.signUpForm.valid){
+      // keep here logic for signUp
+      console.log(this.signUpForm.value);
+      
+    }else{
+      // keep here logic for invalid form
+      this.validateAllFormFields(this.signUpForm)
+    }
+  }
+  
+  private validateAllFormFields(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true})
+      }else if(control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
+    })
   }
 }
