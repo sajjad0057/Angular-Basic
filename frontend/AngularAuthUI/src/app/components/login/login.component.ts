9719c270097f40eeb7f7e6685507d5858d378d35
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-07 19:07:03 +0600

    make login page UI

--- frontend/AngularAuthUI/src/app/components/login/login.component.ts
+++ frontend/AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  passwordFieldType : string = "password"
+  isFieldTypeText: boolean = false;
+  eyeIcon: string = "fa fa-eye-slash";
 
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  hideShowPass(){
+    this.isFieldTypeText = !this.isFieldTypeText;
+    this.isFieldTypeText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
+    this.isFieldTypeText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+  }
 }
