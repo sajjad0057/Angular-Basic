c765370c022ce00732e01763571392cf8e9ad97c
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-07 22:12:49 +0600

    showing validation/error messages and make validateAllFormFields for validate form field

--- frontend/AngularAuthUI/src/app/components/login/login.component.ts
+++ frontend/AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -28,5 +28,27 @@ export class LoginComponent implements OnInit {
     this.isFieldTypeText = !this.isFieldTypeText;
     this.isFieldTypeText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
     this.isFieldTypeText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value)
+      //send the obj to backend
+    }else{
+      console.log("form is not valid"); 
+      this.validateAllFormFields(this.loginForm)
+      alert("Your form is invalid!");
+    }
+  }
+
+  private validateAllFormFields(formGroup:FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true})
+      }else if(control instanceof FormGroup){
+        this.validateAllFormFields(control)
+      }
+    })
   }
 }
