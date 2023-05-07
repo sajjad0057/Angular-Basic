523cf6274b5f66d1fbf5a874e40dae47bcaf8326
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-07 21:10:06 +0600

    Import ReactiveFormsModule in app.module.ts

--- frontend/AngularAuthUI/src/app/app.module.ts
+++ frontend/AngularAuthUI/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
