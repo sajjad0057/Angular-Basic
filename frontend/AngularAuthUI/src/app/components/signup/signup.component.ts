b666d66aa7c22e9703045767a652962886cfdc67
sajjad0057 <sajjadhossain0057@gmail.com>
2023-05-07 19:20:48 +0600

    make signup page UI

--- frontend/AngularAuthUI/src/app/components/signup/signup.component.ts
+++ frontend/AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  
+  passwordFieldType : string = "password"
+  isFieldTypeText: boolean = false;
+  eyeIcon: string = "fa fa-eye-slash";
 
+  constructor(){
+
+  }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  hideShowPass(){
+    this.isFieldTypeText = !this.isFieldTypeText;
+    this.isFieldTypeText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
+    this.isFieldTypeText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+  }
 }
